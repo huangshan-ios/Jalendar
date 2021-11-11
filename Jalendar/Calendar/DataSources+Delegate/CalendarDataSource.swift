dd14acac0bb16f84c10d8b2f59dd38e92e3093eb
SonHoang <sonhoang.ithubt@gmail.com>
2021-11-12 01:21:32 +0700

    Add default returns for calendar datasource protocol

--- Jalendar/Calendar/DataSources+Delegate/CalendarDataSource.swift
+++ Jalendar/Calendar/DataSources+Delegate/CalendarDataSource.swift
@@ -10,11 +10,25 @@ import UIKit
 public protocol CalendarDataSource {
     
     func heightForWeekdayView() -> CGFloat
-        
+    
     func weekDayView(at index: Int, size: CGSize) -> UIView
     
     func dateView(for index: ItemViewIndex, date: Date, rect: CGRect) -> UIView
     
     func selectedDateView(at rect: CGRect) -> UIView
     
+}
+
+public extension CalendarDataSource {
+    func heightForWeekdayView() -> CGFloat {
+        return 24.0
+    }
+    
+    func weekDayView(at index: Int, size: CGSize) -> UIView {
+        return UIView(frame: CGRect(origin: .zero, size: size))
+    }
+    
+    func selectedDateView(at rect: CGRect) -> UIView {
+        return UIView(frame: rect)
+    }
 }
